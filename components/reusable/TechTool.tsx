4d8d798c219b1ba11febd4b1d4dce551fd621278
thegera4 <84020433+thegera4@users.noreply.github.com>
2023-03-01 15:43:32 -0600

    fix: no animation skills

--- components/reusable/TechTool.tsx
+++ components/reusable/TechTool.tsx
@@ -13,7 +13,7 @@ function TechTool({ directionLeft, path, alt }: Props) {
     <>
     <div className="group relative flex cursor-default">
       <Tooltip tooltip={alt}>
-        <motion.img 
+        {/* <motion.img 
           initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
           transition={{ duration: 0.5 }}
           whileInView={{ x: 0, opacity: 1 }}
@@ -21,11 +21,17 @@ function TechTool({ directionLeft, path, alt }: Props) {
           src={path}
           alt={alt}
           className="object-contain filter group-hover:grayscale 
-          transition duration-300 ease-in-out
-          lg:w-20 lg:h-17 
-          sm:w-14 sm:h-14 
-          xs:w-10 xs:h-10 
-          2xs:w-6 2xs:h-6"
+          transition duration-300 ease-in-out lg:w-20 lg:h-17 
+          sm:w-14 sm:h-14 xs:w-10 xs:h-10 2xs:w-6 2xs:h-6"
+        />*/}
+        <Image
+          src={path}
+          alt={alt}
+          width={100}
+          height={100}
+          className="object-contain filter group-hover:grayscale 
+          transition duration-300 ease-in-out lg:w-20 lg:h-17 
+          sm:w-14 sm:h-14 xs:w-10 xs:h-10 2xs:w-6 2xs:h-6"
         />
       </Tooltip>
     </div>
