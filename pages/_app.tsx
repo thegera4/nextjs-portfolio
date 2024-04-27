ad2cc7b93dd005a288f33934e5bf185931c4ad17
Gerardo Medellin <out_jmedellin@findep.com.mx>
2024-04-26 18:21:28 -0600

    feat: added speed insights vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   )
 }
