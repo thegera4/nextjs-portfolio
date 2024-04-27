533da0b9ac269df00c46777166df785b812e10e4
Gerardo Medellin <out_jmedellin@findep.com.mx>
2024-04-26 18:40:21 -0600

    fix:  insights next<13.3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,16 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { useRouter } from 'next/router';
 
 function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
-      <SpeedInsights />
+      <SpeedInsights route={router.pathname} />
     </>
   )
 }
